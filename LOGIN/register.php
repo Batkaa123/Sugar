e046df2b8ff500fdd224432d46745948d72ee55a
Batka <batb73979@gmail.com>
2023-05-03 22:58:33 +0800

    set new

--- LOGIN/register.php
+++ LOGIN/register.php
@@ -42,7 +42,7 @@
                         //Server settings
                         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
                         $mail->isSMTP();                                            //Send using SMTP
-                        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                        $mail->Host       = 'demoon846@gmail.com';                     //Set the SMTP server to send through
                         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                         $mail->Username   = 'demoon846@gmail.com';                     //SMTP username
                         $mail->Password   = 'xbgdwveoavremxum';                               //SMTP password
